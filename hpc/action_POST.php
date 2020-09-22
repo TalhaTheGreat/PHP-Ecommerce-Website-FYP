e28dc71e0cf8840988b5d4f53555ed01c8c327d3
Talha Nadem <40380763+TalhaTheGreat@users.noreply.github.com>
2020-09-22 13:31:43 +0500

    Files

---
+++ hpc/action_POST.php
@@ -0,0 +1,12 @@
+<!--?php
+    // Retrieve order
+    echo 'Hello';
+    $_ResponseMessage = $_REQUEST['pp_ResponseMessage'] ;
+    $_ResponseCode =    $_REQUEST['pp_ResponseCode'];
+    $_InstrToken =      $_REQUEST['pp_InstrToken'];
+    $_RRN = $_REQUEST['pp_RetreivalReferenceNo'];
+    echo $_ResponseMessage;
+    echo $_ResponseCode;
+    echo $_InstrToken;
+    echo $_RRN;
+    ?-->
